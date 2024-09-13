6c4ac0d3d4ef9a7e5b5af8749633bb06a578b7ea
wenzzzel <wenzzzel@live.se>
2024-09-13 21:33:59 +0200

    Added field SantaForId

--- secret-santa-lottery-api/Models/Participant.cs
+++ secret-santa-lottery-api/Models/Participant.cs
@@ -7,11 +7,13 @@ public class Participant
     public string Name { get; set; }
 
     public string Partner { get; set; }
+    public string SantaForId { get; set; }
 
-    public Participant(string id, string name, string partner)
+    public Participant(string id, string name, string partner, string santaForId)
     {
         Id = id;
         Name = name;
         Partner = partner;
+        SantaForId = santaForId;
     }
 }
